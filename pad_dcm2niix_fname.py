094a19843580e16efe178278bc72189633ba9373
Josh Bizzell <jcbizzell@gmail.com>
2021-09-20 13:47:23 -0400

    Changed file separator in pad_dcm2niix_fname.py

--- pad_dcm2niix_fname.py
+++ pad_dcm2niix_fname.py
@@ -14,11 +14,11 @@ args = parser.parse_args()
 # Get the file path and file name
 fpath = os.path.dirname(args.infile)
 fname = os.path.basename(args.infile)
-fileparts = fname.split('---')
+fileparts = fname.split('_')
 # Add zeros in front of series and acquisition 
 fileparts[0]=fileparts[0].zfill(args.srspad)
 fileparts[1]=fileparts[1].zfill(args.acqpad)
 newfile=fileparts[0]
 for s in fileparts[1:]: 
-    newfile=newfile+"---"+s
+    newfile=newfile+"_"+s
 os.rename(args.infile,os.path.join(fpath,newfile))
